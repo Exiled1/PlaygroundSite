85f426df652d89cf8267a2daf230a52587d584c2
Rudy Peralta <rudyperalta831@gmail.com>
2022-04-18 22:31:35 -0700

    Finished the main part of the assignment

--- src/business.rs
+++ src/business.rs
@@ -1,8 +1,10 @@
-use std::fs::File;
+use std::sync::Arc;
 
 use actix_web::{body::BoxBody, http::header::ContentType, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
-use serde_json::{error, from_reader, from_slice, json, Value as JSON};
+use serde_json::json;
+// use std::sync::Arc;
+
 #[derive(Deserialize, Serialize, Clone)]
 pub struct Business {
     pub name: String,
@@ -30,9 +32,183 @@ pub struct Review {
 }
 
 #[derive(Deserialize, Serialize, Clone)]
+pub struct Photo {
+    pub user_name: String,
+    pub photo_id: usize,
+    pub photo_url: String,
+    pub photo_caption: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct UserReviews(Vec<(String, Review)>);
+
+/// The User reviews impl deals with working with reviews on a lower level.
+impl UserReviews {
+    fn new(name: String, review: Review) -> Self {
+        UserReviews(vec![(name, review)])
+    }
+
+    // Adds a review to the list and checks if the user has already reviewed the business. If they have then it will return an error.
+    fn add_review(&mut self, user: String, review: Review) {
+        // If we find any reviews with the same user, then we will return withoutt doing anything.
+        if user == "Anonymous" {
+            self.0.push((user, review));
+        } else if self.0.iter().any(|(user_name, _)| user_name == &user) {
+            return;
+        } else {
+            self.0.push((user, review));
+        }
+    }
+
+    // Finds a review by the user name for a specific business.
+    fn get_review(&self, user_name: String) -> Option<&Review> {
+        Some(
+            &self
+                .0
+                .iter()
+                .find(|(user_name_in_review, _)| user_name_in_review == &user_name)
+                .unwrap()
+                .1,
+        )
+    }
+
+    fn delete_review(&mut self, user: String) {
+        // Checks through the array and deletes anything that matches the username.
+        if user == "Anonymous" {
+            return;
+        } else {
+            self.0.retain(|(user_name, _)| user_name != &user);
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Clone)]
 pub struct BusinessResponse {
     pub business: Business,
-    pub reviews: Option<Vec<Review>>,
+    pub reviews: Option<UserReviews>,
+    pub photos: Option<Vec<Photo>>,
+}
+
+impl BusinessResponse {
+    pub fn new(business: Business, reviews: Option<UserReviews>, photos: Option<Vec<Photo>>) -> Self {
+        BusinessResponse { business, reviews, photos}
+    }
+
+    pub fn delete_business_review(&mut self, user: String) -> HttpResponse {
+        if let Some(reviews) = &mut self.reviews {
+            reviews.delete_review(user.clone());
+            HttpResponse::Ok().json(json!({
+                "message": "Review deleted.",
+                "deleted_review": user,
+            }))
+        } else {
+            HttpResponse::Ok().json(json!({
+                "message": "No reviews to delete.",
+            }))
+        }
+    }
+
+    pub fn add_business_review(&mut self, user: String, review: Review) -> HttpResponse {
+        if let Some(reviews) = &mut self.reviews {
+            reviews.add_review(user.clone(), review.clone());
+            HttpResponse::Ok().json(json!({
+                "message": "Review added.",
+                "added_review": user,
+            }))
+        } else {
+            HttpResponse::Ok().json(json!({
+                "message": "No reviews to add.",
+            }))
+        }
+    }
+
+    pub fn update_business_review(&mut self, user: String, review: Review) -> HttpResponse {
+        if let Some(reviews) = &mut self.reviews {
+            reviews.delete_review(user.clone());
+            reviews.add_review(user.clone(), review.clone());
+            HttpResponse::Ok().json(json!({
+                "message": "Review updated.",
+                "updated_review": review,
+            }))
+        } else {
+            HttpResponse::Ok().json(json!({
+                "message": "No reviews to update.",
+            }))
+        }
+    }
+
+    pub fn get_business_reviews(&self) -> HttpResponse {
+        if let Some(reviews) = &self.reviews {
+            HttpResponse::Ok().json(reviews)
+        } else {
+            HttpResponse::Ok().json(json!({
+                "message": "No reviews to get.",
+            }))
+        }
+    }
+
+    pub fn show_business_reviews(&self, user_name: String) -> HttpResponse {
+        if let Some(reviews) = &self.reviews {
+            let mut reviews_to_show = reviews.0.clone();
+            let mut user_reviews = reviews_to_show
+                .iter()
+                .filter(|(name, review)| name == &user_name)
+                .collect::<Vec<_>>();
+            user_reviews
+                .sort_by(|(_, review_1), (_, review_2)| review_1.rating.cmp(&review_2.rating));
+            // let index_at = page * per_page;
+
+            HttpResponse::Ok().json(user_reviews)
+        } else {
+            HttpResponse::Ok().json(json!({
+                "message": "No reviews to get.",
+            }))
+        }
+    }
+
+    pub fn add_business_photo(&mut self, user_name: String, photo: Photo) -> HttpResponse {
+        if let Some(photos) = &mut self.photos {
+            photos.push(photo.clone());
+            HttpResponse::Ok().json(json!({
+                "message": "Photo added.",
+                "added_photo": photo.clone(),
+            }))
+        } else {
+            HttpResponse::Ok().json(json!({
+                "message": "No photos to add.",
+            }))
+        }
+    }
+
+    pub fn delete_business_photo(&mut self, user_name: String, photo_id: usize) -> HttpResponse {
+        if let Some(photos) = &mut self.photos {
+            photos.retain(|photo| photo.user_name != user_name && photo.photo_id != photo_id);
+            HttpResponse::Ok().json(json!({
+                "message": "Photo deleted.",
+                "deleted_photo": photo_id,
+            }))
+        } else {
+            HttpResponse::Ok().json(json!({
+                "message": "No photos to delete.",
+            }))
+        }
+    }
+
+    pub fn update_business_photo(&mut self, user_name: String, photo: Photo) -> HttpResponse {
+        if let Some(photos) = &mut self.photos {
+            
+            photos.retain(|photo| photo.user_name != user_name);
+            photos.push(photo.clone());
+            HttpResponse::Ok().json(json!({
+                "message": "Photo updated.",
+                "updated_photo": photo,
+            }))
+        } else {
+            HttpResponse::Ok().json(json!({
+                "message": "No photos to update.",
+            }))
+        }
+    }
 }
 
 impl Responder for BusinessResponse {
@@ -46,4 +222,3 @@ impl Responder for BusinessResponse {
             .body(body)
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ mod endpoints;
 use std::collections::HashMap;
 use std::sync::Arc;
 
-use actix_web::http::header::ContentType;
-use business::{Business, BusinessResponse, Review};
+use business::BusinessResponse;
 use serde_json::json;
 // use reviews::Review;
 use actix_web::{
@@ -14,6 +13,8 @@ use actix_web::{
 };
 use tokio;
 use tokio::sync::RwLock;
+
+use crate::business::{Review, Photo};
 
 // use crate::endpoints::AppError;
 
@@ -27,7 +28,6 @@ struct AppState {
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     create_server()?.await?;
-
     Ok(())
 }
 
@@ -45,7 +45,16 @@ fn create_server() -> std::io::Result<Server> {
             .service(add_business)
             .service(get_businesses)
             .service(delete_business)
-            .service(set_business)
+            .service(find_business)
+            .service(web::scope("/app")
+                .service(add_review)
+                .service(delete_review)
+                .service(update_review))
+            .service(web::scope("/photos")
+                .service(add_photo)
+                .service(delete_photo)
+                .service(update_photo))
+
     };
     // App setup ^^^
 
@@ -104,7 +113,7 @@ async fn delete_business(
 }
 
 #[get("/business/{business_name}")]
-async fn set_business(
+async fn find_business(
     business_name: web::Path<String>,
     resources: web::Data<AppState>,
 ) -> std::io::Result<impl Responder> {
@@ -114,6 +123,7 @@ async fn set_business(
         .await
         .get(&business_name.to_string())
         .cloned();
+
     match searched_business {
         Some(business) => Ok(HttpResponse::Ok().json(business)),
         None => Ok(HttpResponse::NotFound().json(json!({
@@ -130,16 +140,177 @@ async fn update_business(
     resources: web::Data<AppState>,
 ) -> std::io::Result<impl Responder> {
     let database = resources.mock_database.clone();
+    // Might be a good idea to make this read to check if it exists instead of potentially taking up a writing spot in the queue.
     let updated_business = database
         .write()
         .await
         .insert(business_name.to_string(), business_data.clone());
+    
     match updated_business {
-        Some(business) => Ok(HttpResponse::Ok().json(business)),
-        None => Ok(HttpResponse::NotFound().json(json!({
-            "notes": "Reached the business update endpoint",
-            "error": "Business not found"
+        Some(business) => Ok(HttpResponse::Ok().json(json!({
+            "success": true,
+            "body": "Replaced an old business!",
+            "previous_business": business,
+            "new_business": business_data
         }))),
+        None => Ok(HttpResponse::Ok().json(json!({ // Returns None if the key didn't exist. It still added a new one.
+            "success": true,
+            "body": "Created a new business!",
+            "created_business": business_data
+        })))
+    }
+}
+// --- Reviews below ---
+
+/// Add a new review to a business. If the content is the exact same, make two seperate reviews.
+#[post("/{reviewer_name}/{business_name}")]
+async fn add_review(
+    params: web::Path<(String, String)>,
+    review_data: web::Json<Review>,
+    resources: web::Data<AppState>,
+) -> std::io::Result<impl Responder> {
+    let (mut reviewer_name, business_name) = params.into_inner();
+    // Yeah, this is unintended behavior. Since I should always assume that a user sends their name in the request.
+    // Currently if a user name is Anonymous, it gets treated differently on purpose. Anon reviews are not allowed to be deleted or edited.
+    if reviewer_name.is_empty() {
+        reviewer_name = "Anonymous".to_string();
+    }
+    let database = resources.mock_database.clone();
+    let mut database_read = database.write().await;
+    let business_data = database_read.get_mut(&business_name);
+    // Check to see if the business exists, if not then return an error.
+    if let Some(business) = business_data {
+        // If the business exists, then retrieve the review list (if it exists).
+        // If it does then add the sent review into the list, if it doesn't, then create the list.
+        // I could probably make it so that the review list always exists, either way works.
+        return Ok(business.add_business_review(reviewer_name, review_data.clone()));
+    } else {
+        return Ok(HttpResponse::NotFound().json(json!({
+            "notes": "Reached the review addition endpoint",
+            "error": "Business not found"
+        })));
+    }
+}
+
+#[delete("/{reviewer_name}/{business_name}")]
+async fn delete_review(
+    params: web::Path<(String, String)>,
+    resources: web::Data<AppState>,
+) -> std::io::Result<impl Responder> {
+    let (reviewer_name, business_name) = params.into_inner();
+    let database = resources.mock_database.clone();
+    let mut database_read = database.write().await;
+    let business_data = database_read.get_mut(&business_name);
+    // Check to see if the business exists, if not then return an error.
+    if let Some(business) = business_data {
+        // If the business exists, then retrieve the review list (if it exists).
+        // If it does then add the sent review into the list, if it doesn't, then create the list.
+        // I could probably make it so that the review list always exists, either way works.
+        return Ok(business.delete_business_review(reviewer_name.clone()));
+    }else {
+        return Ok(HttpResponse::Ok().body(format!("Deleted {reviewer_name}'s review from {business_name}")));
+    }
+}
+
+#[put("/{reviewer_name}/{business_name}")]
+async fn update_review(
+    params: web::Path<(String, String)>,
+    review_data: web::Json<Review>,
+    resources: web::Data<AppState>,
+) -> std::io::Result<impl Responder> {
+    let (reviewer_name, business_name) = params.into_inner();
+    let database = resources.mock_database.clone();
+    let mut database_read = database.write().await;
+    let business_data = database_read.get_mut(&business_name);
+    // Check to see if the business exists, if not then return an error.
+    if let Some(business) = business_data {
+        // If the business exists, then retrieve the review list (if it exists).
+        // If it does then update the sent review into the list, if it doesn't, then exit.
+        return Ok(business.update_business_review(reviewer_name.clone(), review_data.into_inner()));
+    }else {
+        return Ok(HttpResponse::Ok().body(format!("Updated {reviewer_name}'s review from {business_name}")));
+    }
+}
+
+#[get("/{reviewer_name}/{business_name}")]
+async fn show_paginated_reviews(
+    params: web::Path<(String, String)>,
+    resources: web::Data<AppState>,
+) -> std::io::Result<impl Responder> {
+    let (reviewer_name, business_name) = params.into_inner();
+    let database = resources.mock_database.clone();
+    let database_read = database.read().await;
+    let business_data = database_read.get(&business_name);
+    // Check to see if the business exists, if not then return an error.
+    if let Some(business) = business_data {
+        // If the business exists, then retrieve the review list (if it exists).
+        // If it does then add the sent review into the list, if it doesn't, then create the list.
+        // I could probably make it so that the review list always exists, either way works.
+        return Ok(business.show_business_reviews(reviewer_name.clone()));
+    }else {
+        return Ok(HttpResponse::Ok().body(format!("Showing {reviewer_name}'s reviews from {business_name}")));
+    }
+}
+// --- Photos API below ---
+
+#[post("/photos/{user_name}/{business_name}")]
+async fn add_photo(
+    params: web::Path<(String, String)>,
+    photo_data: web::Json<Photo>,
+    resources: web::Data<AppState>,
+) -> std::io::Result<impl Responder> {
+    let (user_name, business_name) = params.into_inner();
+    let database = resources.mock_database.clone();
+    let mut database_read = database.write().await;
+    let business_data = database_read.get_mut(&business_name);
+    // Check to see if the business exists, if not then return an error.
+    if let Some(business) = business_data {
+        // If the business exists, then retrieve the review list (if it exists).
+        // If it does then add the sent review into the list, if it doesn't, then create the list.
+        // I could probably make it so that the review list always exists, either way works.
+        return Ok(business.add_business_photo(user_name.clone(), photo_data.clone()));
+    }else {
+        return Ok(HttpResponse::Ok().body(format!("Added a photo to {business_name}")));
+    }
+}
+
+#[delete("/photos/{user_name}/{business_name}/{photo_id}")]
+async fn delete_photo(
+    params: web::Path<(String, String, usize)>,
+    resources: web::Data<AppState>,
+) -> std::io::Result<impl Responder> {
+    let (user_name, business_name, photo_id) = params.into_inner();
+    let database = resources.mock_database.clone();
+    let mut database_read = database.write().await;
+    let business_data = database_read.get_mut(&business_name);
+    // Check to see if the business exists, if not then return an error.
+    if let Some(business) = business_data {
+        // If the business exists, then retrieve the review list (if it exists).
+        // If it does then add the sent review into the list, if it doesn't, then create the list.
+        // I could probably make it so that the review list always exists, either way works.
+        return Ok(business.delete_business_photo(user_name.clone(), photo_id.clone()));
+    }else {
+        return Ok(HttpResponse::Ok().body(format!("Deleted a photo from {business_name}")));
+    }
+}
+
+#[put("/photos/{user_name}/{business_name}")]
+async fn update_photo(
+    params: web::Path<(String, String)>,
+    photo_data: web::Json<Photo>,
+    resources: web::Data<AppState>,
+) -> std::io::Result<impl Responder> {
+    let (user_name, business_name) = params.into_inner();
+    let database = resources.mock_database.clone();
+    let mut database_read = database.write().await;
+    let business_data = database_read.get_mut(&business_name);
+    // Check to see if the business exists, if not then return an error.
+    if let Some(business) = business_data {
+        // If the business exists, then retrieve the review list (if it exists).
+        // If it does then update the sent review into the list, if it doesn't, then exit.
+        return Ok(business.update_business_photo(user_name.clone(), photo_data.clone()));
+    }else {
+        return Ok(HttpResponse::Ok().body(format!("Updated a photo from {business_name}")));
     }
 }
 
